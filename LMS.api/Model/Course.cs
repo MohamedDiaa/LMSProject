ed10be7b56e337d697756bef858ed4be1a6403e1
Mohamed Diaa <736736+MohamedDiaa@users.noreply.github.com>
2024-06-24 16:57:24 +0200

    add max capacity to course entity

--- LMS.api/Model/Course.cs
+++ LMS.api/Model/Course.cs
@@ -10,6 +10,7 @@ namespace LMS.api.Model
         public int Id { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
+        public int MaxCapcity { get; set; }
 
         public DateTime Start {  get; set; }
 
