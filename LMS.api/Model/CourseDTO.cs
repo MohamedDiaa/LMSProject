2e023c131e6d01391e8a784266a548ee4cd33c65
mpdomon <martin.domon@gmail.com>
2024-06-27 16:37:32 +0200

    Adding course DTO

---
+++ LMS.api/Model/CourseDTO.cs
@@ -0,0 +1,13 @@
+﻿namespace LMS.api.Model
+{
+    public class CourseDTO
+    {
+        public int Id { get; set; }
+        public string? Title { get; set; }
+        public string? Description { get; set; }
+        public int maxCapcity { get; set; } //I spelled it wrong intentionally based on the schema. /Martin 
+        public DateTime Start { get; set; }
+        public DateTime End { get; set; }
+
+    }
+}
