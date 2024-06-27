8b96934ea1857c48e8207e2577513f9fef7ced20
Mattias Hammarhorn <mattias@hammarhorn.se>
2024-06-27 20:03:48 +0200

    Manually remove 'Blazored.Toast' nuget package and all references since I forgot how to rollback properly and am running out of time

--- RoboUnicornsLMS/Program.cs
+++ RoboUnicornsLMS/Program.cs
@@ -1,4 +1,3 @@
-using Blazored.Toast;
 using RoboUnicornsLMS.Components;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -6,8 +5,6 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
-
-builder.Services.AddBlazoredToast();
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7183") });
 
