58e561572fbbbfdc5fa07a1be6113c90d34fc92b
Ulf Bourelius <ulfbourelius71@gmail.com>
2024-06-28 09:01:19 +0200

    Initial commit LMS.api.Services

---
+++ LMS.api/Services/GenericService.cs
@@ -0,0 +1,49 @@
+﻿using LMS.api.Model;
+
+namespace LMS.api.Services
+{
+    using Microsoft.EntityFrameworkCore;
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+
+    public class GenericApiService<TEntity> : IGenericApiService<TEntity> where TEntity : class, IEntity
+    {
+        private readonly LMSContext _context;
+        private readonly DbSet<TEntity> _dbSet;
+
+        public GenericApiService(LMSContext context)
+        {
+            _context = context;
+            _dbSet = context.Set<TEntity>();
+        }
+
+        public async Task<List<TEntity>> GetAllAsync()
+        {
+            return await _dbSet.ToListAsync();
+        }
+
+        public async Task<TEntity> GetByIdAsync(int id)
+        {
+            return await _dbSet.FindAsync(id);
+        }
+
+        public async Task AddAsync(TEntity entity)
+        {
+            await _dbSet.AddAsync(entity);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateAsync(TEntity entity)
+        {
+            _context.Entry(entity).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(int id)
+        {
+            var entity = await _dbSet.FindAsync(id);
+            _dbSet.Remove(entity);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
---
+++ LMS.api/Services/IGenericApiService.cs
@@ -0,0 +1,13 @@
+﻿using LMS.api.Model;
+
+namespace LMS.api.Services
+{
+    public interface IGenericApiService<TEntity> where TEntity : class, IEntity
+    {
+        Task<List<TEntity>> GetAllAsync();
+        Task<TEntity> GetByIdAsync(int id);
+        Task AddAsync(TEntity entity);
+        Task UpdateAsync(TEntity entity);
+        Task DeleteAsync(int id);
+    }
+}
