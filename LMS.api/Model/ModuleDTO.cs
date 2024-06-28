bae95c95acbc239ded9474c8cd67717b1356396d
Mattias Hammarhorn <mattias@hammarhorn.se>
2024-06-28 11:33:15 +0200

    Added Required dataannotations to all ModuleDTO properties and validation in ModuleEdit.razor

--- LMS.api/Model/ModuleDTO.cs
+++ LMS.api/Model/ModuleDTO.cs
@@ -5,13 +5,20 @@ namespace LMS.api.Model
     public class ModuleDTO
     {
         public int Id { get; set; }
+        
+        [Required]
         public string Title { get; set; }
+        
+        [Required]
         public string Description { get; set; }
 
+        [Required]
         public DateTime Start { get; set; }
 
+        [Required]
         public DateTime End { get; set; }
 
+        [Required]
         public int CourseID { get; set; }
 
         public Course? Course { get; set; }
