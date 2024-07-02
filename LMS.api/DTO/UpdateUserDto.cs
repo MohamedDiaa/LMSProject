d4706bfbfc426de8871abee9cbd215c7c07a8fed
Ulf Bourelius <ulfbourelius71@gmail.com>
2024-07-02 14:21:27 +0200

    Fixed Update User Bug

--- LMS.api/DTO/UpdateUserDto.cs
+++ LMS.api/DTO/UpdateUserDto.cs
@@ -3,7 +3,6 @@
     public class UpdateUserDto
     {
         public string Id { get; set; }
-        public string? UserName { get; internal set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string? Email { get; set; }
